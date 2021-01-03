e6456200734211ca14a29b5ed9b38505625900ce
Aman Khalid <aakhalid@umich.edu>
2021-01-02 22:33:17 -0500

    Added some style

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 var metronome = new Metronome();
 var bpm = document.getElementById("BPM");
+var bpm_label = document.getElementById("BPMLabel");
 bpm.value = metronome.bpm;
-bpm.addEventListener("change", function() {
-	if (bpm.value >= 33 && bpm.value <= 200) {
-		metronome.bpm = bpm.value;
-	} else {
-		metronome.bpm = 60;
-	}
-});
 
 var trigger_btn = document.getElementById("triggerBtn");
 trigger_btn.addEventListener("click", function() {
 	metronome.trigger();
-	if (trigger_btn.innerHTML === "Start") {
-		trigger_btn.innerHTML = "Stop";
-	} else {
-		trigger_btn.innerHTML = "Start";
-	}
 });
+
+function change_metronome() {
+	metronome.bpm = bpm.value;
+	bpm_label.innerHTML = bpm.value;
+}
+
+function change_BPM(change) {
+	bpm.stepUp(change);
+	change_metronome();
+}
+
+function set_BPM() {
+	bpm.value = "60";
+	change_metronome();
+}
+
+const links = ["fgWHrYC4LEs?t=240", "rpss7GsCj7A", "zzJzhTqJASY", "h5puAf5jkLc", "hjPrHmDtVGg?t=13", "YQCtq-t0F8U", "qwURbkxB4SU", "sPQ5E4o9Ewk", "j2LYsNujdG0", "eo-_pHMENjQ", "nrUPtkL-6Nk", "yb_5mJlVHPc", "EDh8a-a6TSo", "Jx_tj6EJWOg", "r0JfH_lgDqc?t=400", "UXDVB-glRKw"];
+function inspire() {
+	var choice = Math.floor(Math.random() * links.length);
+	window.open(`https://youtu.be/${links[choice]}`, "_blank");
+}
