7a1cfc6db9963357364e31b9d2c602eedd3da2be
Aman Khalid <aakhalid@umich.edu>
2021-01-07 00:34:59 -0500

    Optimized rep counting algorithm

--- js/counter.js
+++ js/counter.js
@@ -1,9 +1,5 @@
 var mic_input = null;
 var listener = null;
-var notes = 0;
-var tonic = null;
-var top_note = null;
-var last_note = null;
 var tonic_dict = {
 	"A": [3, 3, 2],
 	"A#": [3, 3, 2],
@@ -18,40 +14,25 @@ var tonic_dict = {
 	"G": [3, 3, 2],
 	"G#": [2, 3, 2]
 };
-var note_set = new Set();
-var ideal_set = new Set();
+var check = 0;
+var goal = [];
 
 function log_pitch() {
 	if (!metronome.is_running) {
 		return;
 	}
 
-
 	if (listener.noteName == null) {
 
-	} else if (ideal_set.has(listener.noteName)) {
-		note_set.add(listener.noteName);
+	} else if (listener.noteName == goal[check]) {
+		++check;
 	}
 
-	if (notes == 0 && listener.noteName == tonic) {
-		++notes;
-	} else if (notes == 1 && listener.noteName == top_note) {
-		if (last_note != null && parseInt(top_note.slice(-1)) - parseInt(last_note.slice(-1)) < 2) {
-			++notes;
-		} 
-	} else if (notes == 2 && listener.noteName == tonic && note_set.size == ideal_set.size) {
-		for (var note of ideal_set) {
-			if (!note_set.has(note)) {
-				return;
-			}
-		}
-
-		notes = 0;
-		note_set.clear();
+	if (check == goal.length) {
+		check = 0;
 		counter_label.innerHTML = parseInt(counter_label.innerHTML) + 1;
 	}
 
-	last_note = listener.noteName;
 	requestAnimationFrame(log_pitch);
 }
 
@@ -65,23 +46,26 @@ function start_count() {
 
 	mic_input.play();
 	listener.updatePitch();
-	log_pitch(tonic);
+	log_pitch();
 }
 
 function stop_count() {
 	listener.stopUpdatingPitch();
 	mic_input.stop();
-	notes = 0;
-	note_set.clear();
-	ideal_set.clear();
+	goal = [];
+	check = 0;
 }
 
 function trigger_counter(is_running, tonic_note, num_octaves, instrument) {
 	if (is_running) {
-		tonic = tonic_note + tonic_dict[tonic_note][instrument];
-		top_note = tonic.slice(0, tonic.length - 1) + (parseInt(tonic.slice(-1)) + parseInt(num_octaves));
-		for (let i = 0; i <= num_octaves; ++i) {
-			ideal_set.add(tonic_note + (tonic_dict[tonic_note][instrument] + i));
+		let tonic = tonic_note + tonic_dict[tonic_note][instrument];
+		let i;
+		for (i = 0; i <= num_octaves; ++i) {
+			goal.push(tonic_note + (tonic_dict[tonic_note][instrument] + i));
+		}
+
+		for (i = goal.length - 2; i >= 0; --i) {
+			goal.push(tonic_note + (tonic_dict[tonic_note][instrument] + i));
 		}
 		start_count();
 	} else {
